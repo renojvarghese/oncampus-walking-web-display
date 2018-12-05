b9a2e9ed8b4518d2cb6d905ec2495b33ae18b428
Renoj Varghese <renoj.varghese@uconn.edu>
2018-12-05 10:57:06 -0500

    integrating participants into app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import "./scss/App.scss";
 import SearchBar from "./components/searchBar";
+import ParticipantContainer from "./components/participantContainer";
 class App extends Component {
     render() {
         return (
             <div className="App">
                 Hello World!
                 <SearchBar />
+                <ParticipantContainer />
             </div>
         );
     }
