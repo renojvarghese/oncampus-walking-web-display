6a31982994362415e2f0d588888684e35ea89e78
Renoj Varghese <renoj.varghese@uconn.edu>
2018-12-05 10:48:08 -0500

    importing search box

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./scss/App.scss";
-
+import SearchBar from "./components/searchBar";
 class App extends Component {
     render() {
-        return <div className="App">Hello World!</div>;
+        return (
+            <div className="App">
+                Hello World!
+                <SearchBar />
+            </div>
+        );
     }
 }
 
