c39fb564a8f7132b7f9d9789e846483aa8d23453
Renoj Varghese <renoj.varghese@uconn.edu>
2019-02-27 14:31:39 -0500

    adding static

---
+++ precache-manifest.6844050172e73e853dd025e4c4e556af.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "64eba4cce93f9d1a0a1e",
+    "url": "/static/js/main.64eba4cc.chunk.js"
+  },
+  {
+    "revision": "4d4949a9a9f903a5cc4a",
+    "url": "/static/js/1.4d4949a9.chunk.js"
+  },
+  {
+    "revision": "64eba4cce93f9d1a0a1e",
+    "url": "/static/css/main.eff8df7e.chunk.css"
+  },
+  {
+    "revision": "8a02b9c866cef5ed25295b075fce14e9",
+    "url": "/index.html"
+  }
+];
